ff43778d2b4c8c08b482f786483c57cda3cef559
eno1220 <eno1220dev@gmail.com>
2022-12-05 18:20:24 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,6 +59,89 @@ export default function Home() {
     }
   }, []);
 
+  function addTag(tag: string) {
+    let content = document.querySelector("textarea");
+
+    if (!content) {
+      return;
+    }
+
+    let sentence = content.value;
+    let len = sentence.length;
+    let pos = content.selectionStart;
+    let last = content.selectionEnd;
+
+    let before = sentence.substring(0, pos);
+    let word = "";
+    switch (tag) {
+      case "bold":
+        word = "**";
+        break;
+      case "italic":
+        word = "*";
+        break;
+      case "strikethrough":
+        word = "~~";
+        break;
+      case "inlinecode":
+        word = "`";
+        break;
+      case "link":
+        word = "[]()";
+        break;
+      case "image":
+        word = "![]()";
+        break;
+      case "backquote":
+        word = "> ";
+        break;
+      case "codeblock":
+        word = "```\n\n```";
+        break;
+      case "ul":
+        word = "- ";
+        break;
+      case "ol":
+        word = "1. ";
+        break;
+      case "hr":
+        word = "---";
+        break;
+      case "h1":
+        word = "# ";
+        break;
+      case "h2":
+        word = "## ";
+        break;
+      default:
+        break;
+    }
+
+    if (last == pos) {
+      sentence = before + word + sentence.substring(last, len);
+    } else {
+      sentence =
+        before +
+        word +
+        sentence.substring(pos, last) +
+        word +
+        sentence.substring(last, len);
+    }
+
+    content.value = sentence;
+  }
+
+  function resetText() {
+    localStorage.removeItem("value");
+    reset({ body: "" });
+    let content = document.querySelector("textarea");
+
+    if (!content) {
+      return;
+    }
+    content.value = "";
+  }
+
   async function download() {
     const blob = new Blob([contents.body], {
       type: "text/markdown",
@@ -131,27 +214,113 @@ export default function Home() {
         <div
           className={`${
             isPreview && width < 1024 ? "hidden" : ""
-          } relative px-6 py-10 shadow-xl rounded-md h-full w-full bg-white`}
+          } h-full w-full flex flex-col gap-2`}
         >
-          <div
-            onClick={() => {
-              copyText();
-            }}
-            className="text-slate-300 rounded-tr-md px-2 py-1 hover:bg-slate-100/50 hover:text-slate-400 cursor-pointer text-sm absolute top-0 right-0"
-          >
-            copy
+          <div className="rounded-md w-full text-sm gap-2  py-2 px-6 flex flex-row bg-white shadow">
+            <div
+              className="hover:bg-blue-100 h-5 rounded w-5"
+              onClick={() => {
+                addTag("bold");
+              }}
+            >
+              <img src="/format_bold_FILL0_wght400_GRAD0_opsz48.svg" />
+            </div>
+            <div
+              className="hover:bg-blue-100 h-5 rounded w-5"
+              onClick={() => {
+                addTag("italic");
+              }}
+            >
+              <img src="/format_italic_FILL0_wght400_GRAD0_opsz48.svg" />
+            </div>
+            <div
+              className="hover:bg-blue-100 h-5 rounded w-5"
+              onClick={() => {
+                addTag("inlinecode");
+              }}
+            >
+              <img src="/code_FILL0_wght400_GRAD0_opsz48.svg" />
+            </div>
+            <div
+              className="hover:bg-blue-100 h-5 rounded w-5"
+              onClick={() => {
+                addTag("codeblock");
+              }}
+            >
+              <img src="/code_blocks_FILL0_wght400_GRAD0_opsz48.svg" />
+            </div>
+            <div
+              className="hover:bg-blue-100 h-5 rounded w-5"
+              onClick={() => {
+                addTag("link");
+              }}
+            >
+              <img src="/link_FILL0_wght400_GRAD0_opsz48.svg" />
+            </div>
+            <div
+              className="hover:bg-blue-100 h-5 rounded w-5"
+              onClick={() => {
+                addTag("image");
+              }}
+            >
+              <img src="/image_FILL0_wght400_GRAD0_opsz48.svg" />
+            </div>
+            <div
+              className="hover:bg-blue-100 h-5 rounded w-5"
+              onClick={() => {
+                addTag("ul");
+              }}
+            >
+              <img src="/list_FILL0_wght400_GRAD0_opsz48.svg" />
+            </div>
+            <div
+              className="hover:bg-blue-100 h-5 rounded w-5"
+              onClick={() => {
+                addTag("ol");
+              }}
+            >
+              <img src="/format_list_numbered_FILL0_wght400_GRAD0_opsz48.svg" />
+            </div>
+            <div
+              className="hover:bg-blue-100 h-5 rounded w-5"
+              onClick={() => {
+                addTag("backquote");
+              }}
+            >
+              <img src="/format_quote_FILL0_wght400_GRAD0_opsz48.svg" />
+            </div>
+            <div
+              onClick={() => {
+                resetText();
+              }}
+              className=" h-5 w-5 rounded ml-auto hover:bg-slate-100/50 hover:text-slate-400 cursor-pointer"
+            >
+              <img src="/restart_alt_FILL0_wght400_GRAD0_opsz48.svg" />
+            </div>
+            <div
+              onClick={() => {
+                copyText();
+              }}
+              className=" h-5 w-5 rounded hover:bg-slate-100/50 hover:text-slate-400 cursor-pointer text-sm"
+            >
+              <img src="/file_copy_FILL0_wght400_GRAD0_opsz48.svg" />
+            </div>
           </div>
-          <textarea
-            id="body"
-            spellCheck="false"
-            className="overflow-y-scroll hidden-scrollbar h-full w-full resize-none focus:outline-none bg-white w-full h-full rounded-md text-gray-900"
-            {...register("body")}
-          ></textarea>
-          <div className="text-right text-sm text-slate-300">
-            {contents.body?.trim().length}word
-            {contents.body?.trim().length > 2 ? "s" : ""}
+
+          <div className="bg-white h-full w-full px-6 pt-4 pb-10 shadow-xl rounded-md">
+            <textarea
+              id="body"
+              spellCheck="false"
+              className="overflow-y-scroll hidden-scrollbar h-full w-full resize-none focus:outline-none bg-white w-full h-full rounded-md text-gray-900"
+              {...register("body")}
+            ></textarea>
+            <div className="text-right text-sm text-slate-300">
+              {contents.body?.trim().length}word
+              {contents.body?.trim().length > 2 ? "s" : ""}
+            </div>
           </div>
         </div>
+
         <div
           className={`${
             !isPreview && width < 1024 ? "hidden" : ""
