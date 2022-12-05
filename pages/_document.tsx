bf07a4deeb09c561c06463b482d4986cb36b7879
eno1220 <eno1220dev@gmail.com>
2022-12-05 15:37:06 +0900

    update

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { useRouter } from "next/router";
+
+const MyDocument = () => {
+  return (
+    <Html lang="ja-JP">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default MyDocument;
