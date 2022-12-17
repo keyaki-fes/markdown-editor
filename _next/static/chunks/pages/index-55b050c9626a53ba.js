0c3bed12c9ca0896ac7c442f5e14d92f716cb4d9
eno1220 <eno1220@users.noreply.github.com>
2022-12-17 07:30:25 +0000

    deploy: d9475ffb4f3337486ee09ed4e937dc9548e36af4

---
+++ _next/static/chunks/pages/index-55b050c9626a53ba.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{48312:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(14186)}])}},function(n){n.O(0,[714,895,638,774,888,179],function(){return n(n.s=48312)}),_N_E=n.O()}]);
